3eab9b09b312c610b241d64abc0991cfc9dc9480
mirceamp <mirceamp@DESKTOP-VHG1F6N>
2023-04-25 23:21:06 +0300

    Added 1st lab & README

---
+++ Labs/lab1.hs
@@ -0,0 +1,41 @@
+-- INTRO TO HASKELL
+
+import Data.List 
+
+myInt = 5555555555555555555555555555555555555555555555555555555555555555555555555555555555555
+
+double :: Integer -> Integer 
+double x = x + x 
+
+triple :: Integer -> Integer 
+triple x = x + x + x 
+
+penta :: Integer -> Integer 
+penta x = x + x + x + x + x 
+
+maxim x y = if (x > y)
+                then x 
+            else 
+                y 
+
+max3 x y z = let 
+            u = maxim x y 
+            in (maxim u z)
+
+-- sum of squares of two numbers 
+squareSum :: Integer -> Integer -> Integer 
+squareSum x y = x^2 + y^2 
+
+-- checks if a number is odd or even 
+isEven :: Integer -> String 
+isEven n = if n `mod` 2 == 0 then "even" else "odd"
+
+-- factorial of a number 
+factorial :: Integer -> Integer 
+factorial n = product [1..n]
+
+-- checks if the 1st parameter is greater than the double of the 2nd parameter
+verifyDouble :: Integer -> Integer -> Bool 
+verifyDouble a b = a > double 
+    where double = 2 * b
+
---
+++ README.md
@@ -0,0 +1,3 @@
+## Haskell Learning Journey: Functional Programming Course
+
+This repository documents my journey of learning Haskell by working through the Functional Programming course at the University of Bucharest's Computer Science Program
